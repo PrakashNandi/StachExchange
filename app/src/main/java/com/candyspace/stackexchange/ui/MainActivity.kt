f0e89f02ea3f737ebfc47690235c701c35a22f7a
Prakash Nandi <prakash.vnandi@gmail.com>
2021-01-30 22:51:39 +0000

    Updated the folder structure

--- app/src/main/java/com/candyspace/stackexchange/ui/MainActivity.kt
+++ app/src/main/java/com/candyspace/stackexchange/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.candyspace.stackexchange
+package com.candyspace.stackexchange.ui
 
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -6,6 +6,7 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.candyspace.stackexchange.R
 
 class MainActivity : AppCompatActivity() {
 
